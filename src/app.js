64f4b9617691bfd79e058047771159e7a722a9ae
astha-gh <asthadevadiga7@gmail.com>
2026-04-03 13:16:31 +0530

    chore: initial project setup with express and mongodb

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API running...");
+});
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
+    })
+    .catch(err => console.log(err));
