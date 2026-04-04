3188d0df22f9e7afe09ae7aa97c3fc312881709e
astha-gh <asthadevadiga7@gmail.com>
2026-04-04 14:33:18 +0530

    final: ready for deployment

--- src/app.js
+++ src/app.js
@@ -12,8 +12,6 @@ app.use(express.json());
 
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
-const testRoutes = require("./routes/testRoutes");
-app.use("/api/test", testRoutes);
 const transactionRoutes = require("./routes/transactionRoutes");
 app.use("/api/transactions", transactionRoutes);
 const dashboardRoutes = require("./routes/dashboardRoutes");
